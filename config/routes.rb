024b35951ac4e93d72ec76b62facd806f0eb31d2
mmmmqc <mmmmqc@gmail.com>
2012-07-06 17:32:52 -0700

    add user controller, sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SampleApp::Application.routes.draw do
   get "users/new"
+  root :to => 'static_pages#home'
 
   match '/help',  to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup', to: 'users#new'
   
 
 
@@ -53,10 +55,6 @@ SampleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
